e0e7afb143fe6074fcac771de70867626bc64893
kiho <kiho.kil@gmail.com>
2023-06-02 11:18:05 +0900

    [master] Similarity Checker 1st commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "../SimilarityChecker/similarityChecker.cpp"
+
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+
+}
---
+++ SimilarityChecker/similarityChecker.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class SimilarityChecker
+{
+public:
+
+private:
+
+};
